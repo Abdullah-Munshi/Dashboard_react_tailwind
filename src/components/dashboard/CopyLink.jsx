19ee70bc4650ca32c9ff7049a8de2abe40c16278
Abdullah Munshi <stackbind@gmail.com>
2023-12-20 12:02:44 +0600

    dashboard page done

--- src/components/dashboard/CopyLink.jsx
+++ src/components/dashboard/CopyLink.jsx
@@ -1,12 +1,38 @@
 import React from "react";
-import { Link } from "react-router-dom";
 const CopyLink = ({ title, link }) => {
   return (
-    <div className='space-y-1'>
-      <h4 className='text-primary text-[22px] font-khand font-bold'>{title}</h4>
-      <Link className='text-white text-lg font-medium inline-block'>
-        {link}
-      </Link>
+    <div className="relative p-4 md:p-6 pr-20 gird grid-cols-[auto_60px] items-center bg-[rgba(3,0,12,0.72)] border-[3px] border-orange rounded-2xl_next">
+      <div>
+        <h4 className="text-orange font-orbitron txt_shadow_one text-xl xl:text-[28px] font-bold">
+          {title}
+        </h4>
+        <input
+          className="bg-transparent border-0 outline-none text-white text-[15px] font-orbitron w-full h-7"
+          type="text"
+          value={link}
+          readOnly
+        />
+      </div>
+
+      <button className="absolute right-[8px] md:right-[14px] top-1/2 -translate-y-1/2 ml-auto">
+        <svg
+          width="91"
+          height="91"
+          viewBox="0 0 91 91"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-10 md:w-[56px] h-10 md:h-[56px]"
+        >
+          <path
+            d="M68.2494 68.2502H34.1243C29.9535 68.2502 26.541 64.8377 26.541 60.6668V15.1668C26.541 10.996 29.9535 7.5835 34.1243 7.5835H68.2494C72.4202 7.5835 75.8327 10.996 75.8327 15.1668V60.6668C75.8327 64.8377 72.4202 68.2502 68.2494 68.2502ZM34.1243 15.1668V60.6668H68.2494V15.1668H34.1243Z"
+            fill="#40C5E6"
+          />
+          <path
+            d="M56.875 83.4167H18.9583C14.7875 83.4167 11.375 80.0042 11.375 75.8333V26.5417C11.375 24.2667 12.8917 22.75 15.1667 22.75C17.4417 22.75 18.9583 24.2667 18.9583 26.5417V75.8333H56.875C59.15 75.8333 60.6667 77.35 60.6667 79.625C60.6667 81.9 59.15 83.4167 56.875 83.4167Z"
+            fill="#40C5E6"
+          />
+        </svg>
+      </button>
     </div>
   );
 };
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,26 +1,104 @@
 import React from "react";
 import bgDashboard from "../assets/bg-dashboard.webp";
 import Header from "../layout/Header";
-import { TitlePrimary } from "../components/Utility";
-
 import CopyLink from "../components/dashboard/CopyLink";
+import disk from "../assets/disk-1.png";
+import minus from "../assets/minus.png";
+import plus from "../assets/plus.png";
+
+const BuyNow = () => {
+  return (
+    <button className="w-full h-full border-[3px] sm:border-[5px] border-white bg-[#211F1D] rounded-full inline-flex flex-col items-center justify-center leading-tight sm:leading-[1] font-orbitron text-white text-xl xl:text-[28px] uppercase transition duration-300 hover:border-primary hover:text-primary">
+      <span className="block text-base xl:text-2xl">BUY</span>
+      NOW
+    </button>
+  );
+};
+
+const ClaimNow = () => {
+  return (
+    <button className="w-full h-full border-[3px] sm:border-[5px] border-white bg-[#211F1D] rounded-full inline-flex flex-col items-center justify-center leading-tight sm:leading-[1] font-orbitron text-white text-xl xl:text-[28px] uppercase transition duration-300 hover:border-primary hover:text-primary">
+      <span className="block text-base xl:text-2xl">CLAIM</span>
+      NOW
+    </button>
+  );
+};
 
 const Dashboard = () => {
   return (
-    <div className="bg-[#0B002B] relative font-khand px-4 pb-4 md:p-6 pt-4">
+    <div className="bg-[#0B002B] relative font-khand px-4 pb-4 md:p-6 pt-4 overflow-hidden">
       <div
         className="w-full h-full absolute top-0 start-0 bottom-0 end-0 bg-cover bg-center z-0"
         style={{ backgroundImage: `url(${bgDashboard})` }}
       >
         <span className="w-full h-full bg-one block absolute top-0 left-0 right-0 bottom-0"></span>
       </div>
-      <div className="mb-[120px]">
+      <div className="mb-[30px] sm:mb-[130px] md:mb-[120px]">
         <Header />
       </div>
-      <main className="overflow-hidden relative px-4 py-6 md:p-6 md:pb-10 bg-one broder-[3px] border-[#E9E38B] rounded-xl drop-shadow-[0px_19px_20px_rgba(0,0,0,0.30)]">
-        <div>
-          <div className="space-y-8 md:space-y-0 md:flex justify-between items-center mt-3 mb-7 md:mb-10">
-            <TitlePrimary>DASHBOARD</TitlePrimary>
+      <main className="relative z-10 mb-20 sm:mb-0 ">
+        <div className="disk-outer mx-auto max-w-[1200px]">
+          <div className="md:flex justify-between">
+            <div className="disk_wrapper type-2 mx-auto relative">
+              <img className="w-full" src={disk} alt="Disk" />
+              <div className="disk_top text-center absolute left-1/2 -translate-x-1/2 z-10">
+                <h4 className="text-white txt_shadow_one font-orbitron font-bold">
+                  BUY
+                  <span className="block">CONTRACTS</span>
+                </h4>
+              </div>
+              <div className="middle_point absolute left-1/2 -translate-x-1/2 z-10">
+                <BuyNow />
+              </div>
+              <div className="disk_bottom  text-center absolute left-1/2 -translate-x-1/2 z-10">
+                <p className="text-white txt_shadow_one font-oxanium font-bold text-base xl:text-[24px] leading-[1.1]">
+                  Buy 2 for
+                </p>
+                <strong className="text-white block txt_shadow_one font-oxanium font-bold text-3xl xl:text-[42px] leading-tight">
+                  $36
+                </strong>
+
+                <div className="incrementor absolute flex items-center justify-between">
+                  <button className="w-[50px] h-[50px] xl:w-[70px] xl:h-[70px] rounded-full bg-[#FF6D6D] inline-flex items-center justify-center ">
+                    <img className="w-[24px] xl:w-[24px]" src={minus} alt="-" />
+                  </button>
+                  <button className="w-[50px] h-[50px] xl:w-[70px] xl:h-[70px] rounded-full bg-[#49A6FC] inline-flex items-center justify-center">
+                    <img
+                      className="translate-x-[1px] xl:translate-x-[2px] w-[24px] xl:w-[40px]"
+                      src={plus}
+                      alt="+"
+                    />
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="disk_wrapper type-2 mx-auto relative">
+              <img className="w-full" src={disk} alt="Disk" />
+              <div className="disk_top text-center absolute left-1/2 -translate-x-1/2 z-10">
+                <h4 className="text-white txt_shadow_one font-orbitron font-bold">
+                  CLAIM
+                  <span className="block">REWARDS</span>
+                </h4>
+              </div>
+              <div className="middle_point absolute left-1/2 -translate-x-1/2 z-10">
+                <ClaimNow />
+              </div>
+              <div className="disk_bottom  text-center absolute left-1/2 -translate-x-1/2 z-10">
+                <p className="text-white txt_shadow_one font-oxanium font-bold text-base xl:text-[24px] leading-[1.1]">
+                  Rewards Accumulated
+                </p>
+                <strong className="text-white block txt_shadow_one font-oxanium font-bold text-3xl xl:text-[42px] leading-tight">
+                  $36.00
+                </strong>
+              </div>
+            </div>
+          </div>
+
+          <div className="mb-5 max-w-[1054px] mx-auto">
+            <CopyLink
+              title="MY UNIQUE LINK"
+              link="https://www.plan18.io/0x2170ed0880ac9a755fd29b2688956bd959f933f8"
+            />
           </div>
         </div>
       </main>
